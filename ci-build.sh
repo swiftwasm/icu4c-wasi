4775ba80205152a2c42e391603dd64a45bf79e9c
Yuta Saito <kateinoigakukun@gmail.com>
2020-02-16 02:40:44 +0000

    Install LLVM on CI

--- ci-build.sh
+++ ci-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+# install llvm
+sudo apt-get install llvm -y
+
 # install the Wasi SDK
 wget -O wasisdk.deb "https://github.com/swiftwasm/wasi-sdk/releases/download/20190421.6/wasi-sdk_3.19gefb17cb478f9.m_amd64.deb"
 sudo dpkg -i wasisdk.deb
