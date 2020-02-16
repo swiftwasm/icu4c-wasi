171b2a5b03c33d52f9e794843955e8820b8a09b2
Yuta Saito <kateinoigakukun@gmail.com>
2020-02-16 01:33:47 +0000

    Build libicudata.a with actual data content
    libicudata.a should be build as actual data content static library for
    WebAssembly target because WebAssembly doesn't support file system, so
    can't work with --with-data-packaging=files.
    
    In additon, ICU build system doesn't support cross-build and building
    libicudata at once. build_data_with_cross_tools.patch enables this
    variant for Swift WebAssembly

--- build.sh
+++ build.sh
@@ -19,17 +19,19 @@ fi
 rm -rf icu icu_out || true
 tar xf icu4c-64_2-src.tgz
 # build the cross one
-patch -p0 <patch-double-conversion.diff
+patch -p0 < patch-double-conversion.diff
+patch -p0 < build_data_with_cross_tools.patch
 cd icu/source
 cp config/mh-linux config/mh-unknown
+autoconf
 # note: I don't know if the AR and RANLIB is required - but I've had Ubuntu's ar and ranlib produce .a files that wasm-ld won't read.
 ./configure \
 	--host=wasm32-unknown-none --enable-static --with-cross-build="$srcdir/icu_host/source" \
 	--disable-shared \
 	--disable-tools --disable-tests --disable-samples --disable-extras \
 	--prefix="$srcdir/icu_out" \
-	--with-data-packaging=files \
-	CC="$wasisdk/bin/clang" CXX="$wasisdk/bin/clang++" AR="$srcdir/wrap-ar" \
-	CFLAGS="-fuse-ld=$srcdir/fakeld" CXXFLAGS="-fno-exceptions" RANLIB="touch"
+	--with-data-packaging=static \
+	CC="$wasisdk/bin/clang" CXX="$wasisdk/bin/clang++" AR="llvm-ar" \
+	CXXFLAGS="-fno-exceptions" RANLIB="llvm-ranlib"
 make -j4
 make install
