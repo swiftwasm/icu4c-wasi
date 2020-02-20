b86fe6c91b20676ca6fd78709084fa4d088df018
Yuta Saito <kateinoigakukun@gmail.com>
2020-02-20 04:16:05 -0800

    Install latest wasi-sdk

--- ci-build.sh
+++ ci-build.sh
@@ -4,10 +4,19 @@ set -e
 sudo apt-get install llvm -y
 
 # install the Wasi SDK
-wget -O wasisdk.deb "https://github.com/swiftwasm/wasi-sdk/releases/download/20190421.6/wasi-sdk_3.19gefb17cb478f9.m_amd64.deb"
-sudo dpkg -i wasisdk.deb
+wget -O dist-wasi-sdk.tgz.zip "https://github.com/swiftwasm/wasi-sdk/releases/download/0.2.0-swiftwasm/dist-ubuntu-latest.tgz.zip"
+unzip dist-wasi-sdk.tgz.zip -d .
+WASI_SDK_TAR_PATH=$(find . -type f -name "wasi-sdk-*")
+WASI_SDK_FULL_NAME=$(basename $WASI_SDK_TAR_PATH -linux.tar.gz)
+tar xfz $WASI_SDK_TAR_PATH
+mv $WASI_SDK_FULL_NAME ./wasi-sdk
+
+# Link wasm32-wasi-unknown to wasm32-wasi because clang finds crt1.o from sysroot
+# with os and environment name `getMultiarchTriple`.
+ln -s wasm32-wasi wasi-sdk/share/wasi-sysroot/lib/wasm32-wasi-unknown
+
 # build it
 ./get_source.sh
-WASI_SDK=/opt/wasi-sdk ./build.sh
+WASI_SDK=$PWD/wasi-sdk ./build.sh
 # package it
 tar cJf icu4c-wasi.tar.xz icu_out
