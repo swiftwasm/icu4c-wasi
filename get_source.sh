01aa99a95c083c248a6ee2a8d81e483bb535e622
Yuta Saito <kateinoigakukun@gmail.com>
2020-03-14 10:51:01 +0000

    Diet libicudata.a

--- get_source.sh
+++ get_source.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 rm icu4c-64_2-src.tgz || true
+rm icu4c-64_2-data.zip || true
 wget https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz
+wget https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-data.zip
